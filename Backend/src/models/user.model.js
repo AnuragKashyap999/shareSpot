75ea37fa5c93359da591fea8fa97182f9997b5de
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-07-10 23:12:06 +0530

    add usermodel

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose"
+
+const userSchema =new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String  // cloudinary URL 
+    },
+    coverImage:{
+        type:String, // cloudinary URL
+    },
+    bio:{
+        type:String,
+        default:""
+    },
+    postsCount:{
+        type:Number,
+        default:0
+    },
+    refreshToken:{
+        type:String
+    }
+
+},{timepstamps:true})
+
+export const User = mongoose.model("User",userSchema); 
+
+
