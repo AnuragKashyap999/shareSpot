1e61b2ddb2a47a7422b61a0e8b68d2ca7497fffc
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-07-10 23:46:18 +0530

    add comment model

---
+++ Backend/src/models/comment.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose"
+
+const commentSchema = new mongoose.Schema({
+    text:{
+        type:String,
+        trim:true,
+        default:""
+    },
+    mediaUrl:{
+    type: String,
+    default: ""
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+        required:true
+    }
+
+},{timestamps:true});
+
+commentSchema.pre("validate",function(next){
+    const hasText=this.text.trim().length()>0;
+    const hasMedia= this.mediaUrl.trim().length>0;
+
+    if(!hasText && !hasMedia){
+        return next(new Error("Comment must contain either text or media."))
+    }
+
+})
+
+export const Comment = mongoose.model("Comment" , commentschema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Iske andar Mongoose internally kuch aisa karta hai:
+// User Comment Submit
+//         │
+//         ▼
+// Comment.create()
+//         │
+//         ▼
+// pre("validate")
+//         │
+//         ├── Check Text
+//         ├── Check Media
+//         │
+//         ▼
+// Validation Pass?
+//         │
+//    Yes ─────► Save
+//         │
+//    No ─────► Error
