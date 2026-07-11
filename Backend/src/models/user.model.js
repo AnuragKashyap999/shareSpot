0e0f9f22f529f9f364cfc2aa217c96f59cd48796
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-07-12 00:40:58 +0530

    introduce two methods AccessToken , Refresh token

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const userSchema =new mongoose.Schema({
     username:{
         type:String,
@@ -37,6 +38,48 @@ const userSchema =new mongoose.Schema({
     }
 
 },{timepstamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password =await bcrypt.hash(this.password,10)
+    next()
+})
+
+// methods lets you add your own functions to every document created from the schema.
+
+userSchema.methods.isPasswordCorrect= async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 
 export const User = mongoose.model("User",userSchema); 
 
