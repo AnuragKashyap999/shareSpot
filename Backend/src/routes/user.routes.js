6608a6292ed6a443202263b7eff66ed3ed66d1ff
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-07-20 23:14:50 +0530

    add login,logout routes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import {Router}  from "express"
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser, logOut, registerUser } from "../controllers/user.controller.js"
 
 const router = Router()
 
 router.route("/regsiter").post(registerUser)
-
+router.route("/login").post(loginUser)
+router.route("/logout").post(logOut)
 
 export default router
